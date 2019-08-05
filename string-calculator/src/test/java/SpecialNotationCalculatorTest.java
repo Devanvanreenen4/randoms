abb72b91a45b94e448821004f70486ddee741c70
Devan Van Reenen <Devan.VanReenen@fnb.co.za>
2019-08-05 16:56:41 +0200

    Updated Calculator test class so that we initialize the Calculator before executing different test cases in class.

--- string-calculator/src/test/java/SpecialNotationCalculatorTest.java
+++ string-calculator/src/test/java/SpecialNotationCalculatorTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
@@ -5,20 +6,24 @@ import static org.junit.Assert.*;
 
 public class SpecialNotationCalculatorTest {
 
+    private SpecialNotationCalculator calculator;
+
+    @Before
+    public void setup() {
+        calculator = new SpecialNotationCalculator();
+    }
+
     @Test
     public void shouldAddTwoNumbers() {
-        SpecialNotationCalculator calculator = new SpecialNotationCalculator();
         assertThat(calculator.calculate("5 6 +"), is(11));
         assertThat(calculator.calculate("1 1 +"), is(2));
         assertThat(calculator.calculate("1 2 +"), is(3));
         assertThat(calculator.calculate("100 200 +"), is(300));
 
-
     }
 
     @Test
     public void shouldSubtractTwoNumbers() {
-        SpecialNotationCalculator calculator = new SpecialNotationCalculator();
         assertThat(calculator.calculate("2 1 -"), is(1));
         assertThat(calculator.calculate("1 2 -"), is(-1));
     }
