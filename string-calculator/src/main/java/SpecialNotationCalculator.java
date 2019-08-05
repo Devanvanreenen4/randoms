1b6255d48005a778266174974920e09bda102f48
Devan Van Reenen <Devan.VanReenen@fnb.co.za>
2019-08-05 15:10:06 +0200

    Initial Commit

---
+++ string-calculator/src/main/java/SpecialNotationCalculator.java
@@ -0,0 +1,10 @@
+/**
+ * Created by f4725816 on 2019/08/05.
+ */
+public class SpecialNotationCalculator {
+
+    public void execute() {
+
+    }
+
+}
---
+++ string-calculator/src/test/java/SpecialNotationCalculatorTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * Created by f4725816 on 2019/08/05.
+ */
+public class SpecialNotationCalculatorTest {
+
+    @Test
+    public void test() {
+
+    }
+}
