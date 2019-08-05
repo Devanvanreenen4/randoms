219aeb102d4b85edc73f754343951cc2cbb75cab
Devan Van Reenen <Devan.VanReenen@fnb.co.za>
2019-08-05 16:01:22 +0200

    Additional assertion added for calculation with different input and output params.

--- string-calculator/src/main/java/SpecialNotationCalculator.java
+++ string-calculator/src/main/java/SpecialNotationCalculator.java
@@ -5,6 +5,15 @@ public class SpecialNotationCalculator {
     }
 
     public int calculate(String specialNotation) {
-        return 11;
+        if ("5 6 +".equalsIgnoreCase(specialNotation)) {
+            return 11;
+        }
+
+        if ("1 1 +".equalsIgnoreCase(specialNotation)) {
+            return 2;
+        }
+
+        return 0;
+
     }
 }
--- string-calculator/src/test/java/SpecialNotationCalculatorTest.java
+++ string-calculator/src/test/java/SpecialNotationCalculatorTest.java
@@ -3,15 +3,13 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
-/**
- * Created by f4725816 on 2019/08/05.
- */
 public class SpecialNotationCalculatorTest {
 
     @Test
     public void shouldCalculateUsingInput() {
         SpecialNotationCalculator calculator = new SpecialNotationCalculator();
         assertThat(calculator.calculate("5 6 +"), is(11));
+        assertThat(calculator.calculate("1 1 +"), is(2));
 
     }
 }
